0b2d7fafd758143fc71f6ce1a031c7b5aa9be405
desmondvu <d6vu@gmail.com>
2024-04-06 15:26:29 -0700

    features stub

--- dinosaur.py
+++ dinosaur.py
@@ -1,18 +1,30 @@
 class dinosaur:
-    def __init__(self) -> None:
+    def __init__(self, **traits) -> None:
         #Stats
         self.currentHealth = 0
-        self.totalHealth = 0
+        self.totalHealth = 100
         self.carnivore_scalar = 0
         self.herbivore_scalar = 0
         self.speed = 0
         self.energy = 0
-        self.size = 0
         self.energyConsumption = 0
         
         #Features of this dinosaur
-        self.hasSharpTeeth = False
-        self.hasHorns = False
-        self.flying = False
-        self.size = 0 #Big medium large
+        self.defaultTraits = {'Size': None,
+                       'Mouth' : None,
+                       'Mobility' : None,
+                       'Combat' : None,
+                       'Neck' : None,
+                       'Tail' : None,
+                       }
+        self.traits = {**self.defaultTraits, **traits}
+        
+        self.healthModifiers = {'Large' : 1.5,
+                                'Medium' : 1.25,
+                                'Small' : 1.0,
+                                'Bipedal' : 1.0,
+                                'Quadruped' : 1.25,
+                                'Flying': }
+
+    def child(self,other):
         
