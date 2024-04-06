e9b2937b7f51fe211daa3dbebb12c7a0fa03ad6b
desmondvu <d6vu@gmail.com>
2024-04-06 15:47:50 -0700

    continued modifiers

--- dinosaur.py
+++ dinosaur.py
@@ -2,9 +2,9 @@ class dinosaur:
     def __init__(self, **traits) -> None:
         #Stats
         self.currentHealth = 0
-        self.totalHealth = 100
-        self.carnivore_scalar = 0
-        self.herbivore_scalar = 0
+        self.totalHealth = 0
+        self.carnivore_scalar = 1
+        self.herbivore_scalar = 1
         self.speed = 0
         self.energy = 0
         self.energyConsumption = 0
@@ -19,12 +19,42 @@ class dinosaur:
                        }
         self.traits = {**self.defaultTraits, **traits}
         
+        #Setting up Health
         self.healthModifiers = {'Large' : 1.5,
                                 'Medium' : 1.25,
                                 'Small' : 1.0,
                                 'Bipedal' : 1.0,
                                 'Quadruped' : 1.25,
-                                'Flying': }
+                                'Flying': 0.75,
+                                None : 1.0,
+                                'Mobile Tail' : 1.0,
+                                'Attack Tail' : 1.25}
+        self.totalHealth = 100 * self.healthModifiers[self.traits['Size']] * self.healthModifiers[self.traits['Mobility']] * self.healthModifiers[self.traits['Tail']]
+        self.currentHealth = self.totalHealth
+
+        #Setting up speed
+        self.speedModifiers = {'Large' : -1,
+                               'Medium' : 0,
+                               'Small' : 1,
+                               'Bipedal' : 1,
+                               'Quadruped' : -1,
+                               'Flying' : 1,
+                               None : 0,
+                               'Mobile Tail': 1,
+                               'Attack Tail' : -1}
+        self.speed = 4 + self.speedModifiers[self.traits['Size']] + self.speedModifiers[self.traits['Mobility']] + self.speedModifiers[self.traits['Tail']]
+
+        #Setting up energy
+        self.energyModifier = {'Large' : 1.5,
+                               'Medium' : 1.25,
+                               'Small' : 1,
+                               'Bipedal' : 1,
+                               'Quadruped' : 1.25,
+                               'Flying' : 0.75,
+                               None : 0,
+                               'Long': 1}
+        
+    
 
     def child(self,other):
-        
+        ...    
