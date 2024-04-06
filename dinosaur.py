17fc85f3f5013457240d641bc85c21acb283a5a6
gavmere <gavinmeregillano@gmail.com>
2024-04-06 13:49:06 -0700

    dinosaur addition

---
+++ dinosaur.py
@@ -0,0 +1,18 @@
+class dinosaur:
+    def __init__(self) -> None:
+        #Stats
+        self.currentHealth = 0
+        self.totalHealth = 0
+        self.carnivore_scalar = 0
+        self.herbivore_scalar = 0
+        self.speed = 0
+        self.energy = 0
+        self.size = 0
+        self.energyConsumption = 0
+        
+        #Features of this dinosaur
+        self.hasSharpTeeth = False
+        self.hasHorns = False
+        self.flying = False
+        self.size = 0 #Big medium large
+        
