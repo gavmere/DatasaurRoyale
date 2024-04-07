05c5d6e2ad73bfa105ee040d98101915942455ea
jtran-9 <jrtran@ucsd.edu>
2024-04-07 04:33:07 -0700

    added copDino behavior

--- singleEmu.py
+++ singleEmu.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import DinoFunctions
 from GameFunctions import populateDinoList
-from Dinosaur import goToGreensOnly, goToNearestDino, fiftyFftydinoPlans, cowardDino, randomBehav, paralyzed, getStats
+from Dinosaur import goToGreensOnly, goToNearestDino, fiftyFftydinoPlans, cowardDino, randomBehav, paralyzed, getStats, copDino
 
 
 class Fullsimulation:
@@ -34,8 +34,7 @@ class Fullsimulation:
         self.dinos = populateDinoList(16)
         self.dino_pos = []
         self.death = []
-        self.behaviors = [goToGreensOnly, goToNearestDino,
-                          fiftyFftydinoPlans, cowardDino, randomBehav, paralyzed]
+        self.behaviors = [goToGreensOnly, copDino]
         self.dino_behaviorFunc = {i: random.choice(
             self.behaviors) for i in self.dinos}
         self.walls = [(0, i) for i in range(self.grid_height)] + [(self.grid_width - 1, i) for i in range(self.grid_height)] + \
@@ -212,7 +211,7 @@ class Fullsimulation:
             if (dino.dino_x, dino.dino_y) != (currDino.dino_x, currDino.dino_y):
                 other_dino_positions.append((dino.dino_x, dino.dino_y))
                 other_dino_powers.append(dino.power)
-                other_dino_colors.append(dino.dino_color)
+                other_dino_colors.append((dino.dino_color, (dino.dino_x, dino.dino_y)))
                 other_dino_energy_consumption.append(dino.energyConsumption)
                 other_dino_energy.append(dino.energy)
                 other_dino_carn.append(dino.carnVal)
