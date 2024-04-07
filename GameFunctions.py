f4f7e6c1719c4d363a7d8daaca1d45c0cf4683cd
nolancchu <nolancchu@gmail.com>
2024-04-06 17:00:25 -0700

    random pick stubs

--- GameFunctions.py
+++ GameFunctions.py
@@ -2,6 +2,31 @@ import DinoFunctions
 import Dinosaur
 import random
 
+def pickSize():
+    sizeList = ['Small', 'Medium', 'Large']
+    return random.choice(sizeList)
+
+def pickMouth():
+    mouthList = ['Beak', 'Medium', 'Large']
+    return random.choice(mouthList)
+
+def pickMobility():
+    mobilityList = ['Bipedal', 'Quadruped', 'Flying']
+    return random.choice(mobilityList)
+
+def pickCombat():
+    combatList = ['Spikes', 'Horns', 'Claws']
+    return random.choice(combatList)
+
+def pickNeck():
+    neckList = ['longNeck', 'normNeck']
+    return random.choice(neckList)
+
+def pickTail():
+    tailList = ['noTail', 'Mobile Tail', 'Attack Tail']
+    return random.choice(tailList)
+
+
 def createDino():
     newDino = Dinosaur.dinosaur([])
     return newDino
@@ -9,6 +34,6 @@ def createDino():
 def populateDinoList(num, parent=None):
     dinoList = []
     for i in range(num):
-        
+        ...
     return dinoList
     
